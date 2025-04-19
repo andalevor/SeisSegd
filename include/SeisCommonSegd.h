06f800559f15dd2bf154e4e0bbab1cdcbed20b49
andalevor <andalevor@gmail.com>
2025-04-19 13:02:03 +0200

    added mlib submodule

---
+++ include/SeisCommonSegd.h
@@ -0,0 +1,13 @@
+/**
+ * \file SeisCommonSegd.h
+ * \brief Type and functions for common SEGD parts that could be shared.
+ * \author andalevor
+ * \date 2025\04\19
+ */
+
+#ifndef SEIS_COMMON_SEGD_H
+#define SEIS_COMMON_SEGD_H
+
+#include <SeisTrace.h>
+
+#endif /* SEIS_COMMON_SEGD_H */
---
+++ src/SeisCommonSegd.c
@@ -0,0 +1,1 @@
+#include "SeisCommonSegd.h"
